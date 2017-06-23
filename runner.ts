1ad51f41b570b695b06e1bfa569c159197ea5416
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2017-06-24 00:19:23 +0200

    :rocket: decouple requests

--- runner.ts
+++ runner.ts
@@ -4,23 +4,27 @@ import { get } from "get"
 const { host, port = 80, paths, resultsFolder } = require("config.json")
 
 const base = host + ":" + port
-const urls = paths.map((s : string) => base + s)
 const duration = parseInt(process.argv[2])/1000 || 0
 
 const start = Date.now()
 const end = start + duration
 
 const run = async () => {
-  while (Date.now() < end) {
-    await map(paths, async path => {
-      const url = base + path
+  paths.forEach((path : string) => {
+    const url = base + path
+    
+    const handler = async () => {
       console.log("Polling " + url)
+      
       const contents = await get(url)
       console.log(contents)
       const fileName = resultsFolder + Date.now() + "_" + path + ".json"
       writeFile(fileName, contents, "utf8", () => {})
-    })
-  }
+      if (Date.now() < end) handler()
+    }
+
+    handler()    
+  })
 }
 
 run()
